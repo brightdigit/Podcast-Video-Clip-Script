f294dcac3fa1cd64bcd38c0f91924da599743353
Leo Dion <leogdion@brightdigit.com>
2019-07-08 18:45:43 -0400

    refactored sample

---
+++ sample.sh
@@ -0,0 +1,8 @@
+ #!/bin/bash
+EPISODE_NO=17
+EPISODE_NAME="Subcontracting"
+GUEST_APPEND=" with Joe Cieplinski"
+CLIP_NAME="Communication and Relationships"
+AUDIO_FILE="Empower Apps 17 quote 1.mp3"
+
+. ./podcast_video_clips.sh
